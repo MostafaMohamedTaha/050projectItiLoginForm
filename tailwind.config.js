b32d5f96f2a010f5f6a97f340980b3102bdb59f3
MostafaMohamedTaha <88216249+MostafaMohamedTaha@users.noreply.github.com>
2023-07-29 18:50:28 +0300

    adding cart page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode:"class",
   theme: {
+    screens:{
+      'xsm':'350px',
+      'sm':'640px',
+      'md':'840px',
+      'lg':'1920px',
+    },
     extend: {},
   },
   plugins: [],
